6e7ca2eece2ad7ab16007ffa83a81d6a51b9a086
prostosandr <amramabramovic@yandex.ru>
2025-02-13 16:47:37 +0700

    Попытка3

--- Cube_explosions/Assets/Scripts/Cube.cs
+++ Cube_explosions/Assets/Scripts/Cube.cs
@@ -2,22 +2,33 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
+
 public class Cube : MonoBehaviour
 {
-    public event Action<List<Rigidbody>> Spawned;
+    [SerializeField] private float chanceTreshold;
+
+    public event Action<GameObject, float, List<Rigidbody>> Spawned;
     public event Action<List<Rigidbody>> Exploded;
+
+    public void DecreaseChance()
+    {
+        int divisor = 2;
+
+        chanceTreshold /= divisor;
+    }
 
     private void Awake()
     {
-        gameObject.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
     }
 
     private void OnMouseUpAsButton()
     {
-        List<Rigidbody> _objects = new List<Rigidbody>();
+        List<Rigidbody> explodableObjects = new List<Rigidbody>();
 
-        Spawned?.Invoke(_objects);
-        Exploded?.Invoke(_objects);
+        Spawned?.Invoke(gameObject, chanceTreshold, explodableObjects);
+        Exploded?.Invoke(explodableObjects);
         Destroy(gameObject);
     }
 }
--- Cube_explosions/Assets/Scripts/Spawner.cs
+++ Cube_explosions/Assets/Scripts/Spawner.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Cube _cube;
-    [SerializeField] private float chanceTreshold;
 
     private void OnEnable()
     {
-        _cube.Spawned += Spawn; 
+        _cube.Spawned += Spawn;
     }
 
     private void OnDisable()
@@ -16,9 +15,9 @@ public class Spawner : MonoBehaviour
         _cube.Spawned -= Spawn;
     }
 
-    private void Spawn(List<Rigidbody> explodableObjects)
+    private void Spawn(GameObject gameObjectCube, float chanceTreshold, List<Rigidbody> _explodableObjects)
     {
-        System.Random randomNumber = new System.Random();
+        System.Random random = new System.Random();
 
         int divisor = 2;
         int minCubesNumber = 2;
@@ -26,18 +25,20 @@ public class Spawner : MonoBehaviour
         int minChance = 0;
         int maxChance = 100;
 
-        if (randomNumber.Next(minChance, maxChance++) <= chanceTreshold)
-        {
-            int cubesNumber = randomNumber.Next(minCubesNumber, maxCubesNumber++);
+        _cube = gameObjectCube.GetComponent<Cube>();
 
-            chanceTreshold /= divisor;
+        if (random.Next(minChance, maxChance++) <= chanceTreshold)
+        {
+            int cubesNumber = random.Next(minCubesNumber, maxCubesNumber++);
 
             for (int i = 0; i < cubesNumber; i++)
             {
-                GameObject newCube = Instantiate(gameObject);
+                GameObject newCube = Instantiate(gameObjectCube);
                 newCube.transform.localScale /= divisor;
+                newCube.GetComponent<Cube>().DecreaseChance();
+                newCube.GetComponent<Cube>().Spawned += Spawn;
 
-                explodableObjects.Add(newCube.GetComponent<Rigidbody>());
+                _explodableObjects.Add(newCube.GetComponent<Rigidbody>());
             }
         }
     }
